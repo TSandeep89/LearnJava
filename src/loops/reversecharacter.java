921fd2312f5cb8dca6a401f29cf92b32bb48d4f0
thodetisandeep <33202375+thodetisandeep@users.noreply.github.com>
2023-12-29 20:17:29 +0530

    Test Commit Name change

--- src/loops/reversecharacter.java
+++ src/loops/reversecharacter.java
@@ -4,7 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class reversecharacter {
     public static void main(String[] args) {
-        String str = "My name is King Khan";
+        String str = "My name is King Khan ";
         int len = str.length();
         System.out.println(len);
         String spt[] = str.split(" ");
